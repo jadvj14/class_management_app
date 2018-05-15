e3532213f1d389e325106be2fe7a2bb4a338f1ec
James Alain N. Dantes <james@jeonsoft.com>
2018-05-15 18:25:13 +0800

    Seed and instructions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Professor.create(first_name: 'James Alain', last_name: 'Dantes')
